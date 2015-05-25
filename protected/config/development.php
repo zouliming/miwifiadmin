bf8311c09e51190eb492643582a41108c2699774
liming.zou <liming.zou@vipshop.com>
2015-05-25 10:21:33 +0800

    update config

--- protected/config/development.php
+++ protected/config/development.php
@@ -72,7 +72,7 @@ return array(
                         'class' => 'CMemCache',
                         'servers' => array(
                                 array(
-                                        'host' => "10.199.168.255",
+                                        'host' => "127.0.0.1",
                                         'port' => "11211",
                                 )
                         )
